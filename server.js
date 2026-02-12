bd4a3541c82ed3837f211fcd1f52bf96ec801da4
Ashu <68785902+ma290@users.noreply.github.com>
2026-02-12 14:41:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,26 +9,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-// in-memory IP locks
-// production me Redis lagana better hoga
-const ipLocks = {}; // { ip: timestamp }
-
-// browser batata hai: 5 complete
-app.post("/completed", (req, res) => {
-  const ip = req.ip;
-  ipLocks[ip] = Date.now() + 24 * 60 * 60 * 1000; // 24 hours
-  res.json({ status: "locked_for_24h" });
-});
-
-// block locked IPs
-app.use((req, res, next) => {
-  const ip = req.ip;
-  if (ipLocks[ip] && Date.now() < ipLocks[ip]) {
-    return res.status(403).send("Daily limit reached. Try after 24 hours.");
-  }
-  next();
-});
-
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
 });
